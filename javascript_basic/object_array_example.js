8ee3f8a64c9f3ba986d5bc66bab3981ce905e093
seungyeonseong <96861618+seungyeonseong@users.noreply.github.com>
2022-05-17 13:45:16 +0900

    구조분해 문법/템플릿 리터럴

---
+++ javascript_basic/object_array_example.js
@@ -0,0 +1,68 @@
+let car1 = {
+    name : "소나타",
+    ph : 200,
+    start : () => {
+        console.log('engine start');
+    },
+    stop : () => {
+        console.log('engine stop');
+    }
+}
+
+let car2 = {
+    name : "bmw",
+    ph : 500,
+    start : () => {
+        console.log('engine start');
+    },
+    stop : () => {
+        console.log('engine stop');
+    }
+}
+
+let car3 = {
+    name : "mini",
+    ph : 120,
+    start : () => {
+        console.log('engine start');
+    },
+    stop : () => {
+        console.log('engine stop');
+    }
+}
+
+let cars = [car1,car2,car3]
+/*
+if(cars.length > 3){
+    console.log("1대 초과")
+} else{
+    console.log("해당 조건은 거짓입니다")
+}*/
+
+//#work2 of / for / map 상관없이 차량의 이름이 bmw일 경우에 "차량을 찾았습니다"를 출력
+for(let i=0;i<cars.length;i++){
+    if (cars[i].name=="bmw"){
+        console.log("차량을 찾았습니다")
+    }
+}
+
+for(element of cars){
+    if (element.name=="bmw"){
+        console.log("차량을 찾았습니다")
+    }
+}
+
+cars.map(car => {
+    if (car.name==="bmw"){
+        console.log("차량의 이름은 : "+car.name+ " 차량을 찾았습니다. 마력은 " +car.ph +"입니다")
+        console.log(`차량의 이름은 : ${car.name} 차량을 찾았습니다 마력은 ${car.ph}입니다`)
+    }
+})
+
+//구조분해 할당
+//const name = car1.name;
+//const ph = car1.ph;
+//귀찮
+
+const {name,ph} = car1;
+console.log(name,ph);
