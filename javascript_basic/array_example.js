a2c6b29e78a3ef6ef3e7a66e7a08115b7329d8a7
seungyeonseong <96861618+seungyeonseong@users.noreply.github.com>
2022-05-17 13:15:48 +0900

    배열 반복문 예제

--- javascript_basic/array_example.js
+++ javascript_basic/array_example.js
@@ -5,8 +5,29 @@ console.log(array);
 array.push('d');
 
 console.log(array);
+//배열 접근
+console.log(array[1])
+
 
 //in java
 //private String [] StringArray = new String[6];
 //private ArrayList<String> arrayListString = new ArrayList<String>;
+
+
+for(let i =0; i < array.length; i++){
+    console.log(array[i])
+}
+//가장 기본적인 반복문의 표현법
+console.log("------------------------")
+
+for(element of array){
+    console.log(element);
+}
+//for ...of의 사용법
+console.log("------------------------")
+array.map(() => {
+    console.log(element);
+})
+//array.map(function(){})
+console.log("------------------------")
 
