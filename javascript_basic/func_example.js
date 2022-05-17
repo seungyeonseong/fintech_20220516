69eacc1de1f373cc414f3e16d23f07fac8b0f6d4
seungyeonseong <96861618+seungyeonseong@users.noreply.github.com>
2022-05-17 11:32:39 +0900

    arrow function

---
+++ javascript_basic/func_example.js
@@ -0,0 +1,25 @@
+//function plus(p1,p2) {
+//    return p1+p2;
+//}
+
+//arrow function
+const plus = (p1,p2) => {
+    return p1+p2;
+}
+console.log(plus(2,5));
+
+//#work 1 : 빼기, 나누기, 곱하기 기능을 선언 해볼 것
+const subtract = (p1,p2) => {
+    return p1-p2;
+}
+console.log(subtract(6,3))
+
+const div = (p1,p2) => {
+    return p1/p2;
+}
+console.log(div(6,3));
+
+const mul = (p1,p2) => {
+    return p1*p2;
+}
+console.log(mul(6,3));
