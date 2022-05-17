725500cc5e433d0cda48b99a316d993edd574a3b
seungyeonseong <96861618+seungyeonseong@users.noreply.github.com>
2022-05-17 11:18:44 +0900

    var, let, const 차이

---
+++ javascript_basic/variable_example.js
@@ -0,0 +1,25 @@
+//in java
+//private String name = "홍길동";
+//private int age = 34;
+//private double height = 182.5;
+
+//javascript
+var uname = "홍길동";
+var age = 34;
+var height = 13.23;
+
+console.log(uname,age,height);
+
+
+let weight = 66;
+//let weight = 82;
+
+var age = 36;
+console.log(uname,age,height);
+
+const gender = "F";
+//gender = "M";
+
+//let hello = () => {
+//    console.log("hello");
+//}
